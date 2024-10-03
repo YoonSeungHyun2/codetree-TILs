be96337d6e4d7f36c2b104077fe5bcca4ad12b59
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-03 20:35:57 +0900

    Title: 정수 세 개 입력받아 출력, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241003/정수 세 개 입력받아 출력/take-three-integers-and-output.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+c = int(input())
+
+print(a, b, c)
+# print(f"{a} {b} {c}")
+
+# # 첫 번째 줄에서 두 개의 정수를 입력받습니다.
+# a, b = map(int, input().split())
+
+# # 두 번째 줄에서 한 개의 정수를 입력받습니다.
+# c = int(input())
+
+# # 입력받은 값을 공백으로 구분하여 출력합니다.
+# print(f"{a} {b} {c}")
