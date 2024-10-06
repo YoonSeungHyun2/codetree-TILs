85ac5e0c77b0254abc130497ee13b852c4c67dab
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-06 17:45:50 +0900

    Title: 윤년인가, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 241006/윤년인가/is-leap-year.py
+++ 241006/윤년인가/is-leap-year.py
@@ -1,14 +1,17 @@
+# y = int(input())
+
+# if (y % 4 == 0):
+#     print("true")
+# elif (y % 100 == 0) and (y % 400 != 0):
+#     print("false")
+# else:
+#     print("false")
+
+# 자연수 y를 입력받습니다.
 y = int(input())
 
-if (y % 4 == 0):
+# 윤년 판단
+if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
     print("true")
-elif (y % 100 == 0) and (y % 400 != 0):
-    print("false")
 else:
     print("false")
-
-    # 윤년 판단
-# if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
-#     print("윤년입니다.")
-# else:
-#     print("평년입니다.")
