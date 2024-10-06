5a16f7a715487e52f15cf45cf2db785e527cbafb
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-06 17:03:52 +0900

    Title: 숫자의 계절은, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 241006/숫자의 계절은/season-of-num.py
+++ 241006/숫자의 계절은/season-of-num.py
@@ -1,10 +1,10 @@
 a = int(input())
 
-if 3 <= a:
+if 3 <= a <= 5:
     print("Spring")
-elif 6 <= a:
+elif 6 <= a <= 8:
     print("Summer")
-elif 9 <= a:
+elif 9 <= a <= 11:
     print("Fall")
 else:
     print("Winter")
