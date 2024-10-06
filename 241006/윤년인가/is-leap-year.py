3e9d28e4552a2874268c587e9c3e570031243879
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-06 17:43:00 +0900

    Title: 윤년인가, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241006/윤년인가/is-leap-year.py
+++ 241006/윤년인가/is-leap-year.py
@@ -1,11 +1,11 @@
 y = int(input())
 
-if (y % 4 == 0 and y % 100 != 100) or (y % 400 == 0):
+if (y % 4 == 0):
     print("true")
 else:
     print("false")
 
-#     # 윤년 판단
+    # 윤년 판단
 # if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
 #     print("윤년입니다.")
 # else:
