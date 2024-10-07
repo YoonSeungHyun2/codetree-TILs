3b03c5be60e61e0d743ee8bc0cec30b731eafd7c
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-07 14:49:12 +0900

    Title: 100점 도달하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241007/100점 도달하기/reach-100-point.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n, 101):
+    if 100 >= i >= 90:
+        print("A", end = " ")
+    elif 90 > i >= 80:
+        print("B", end = " ")
+    elif 80 > i >= 70:
+        print("C", end = " ")
+    elif 70 > i >= 60:
+        print("D", end = " ")
+    else:
+        print("F", end = " ")
