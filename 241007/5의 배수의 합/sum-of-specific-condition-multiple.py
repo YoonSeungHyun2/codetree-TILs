a692ff7413f0fa5019ed903cb012631142ada7b9
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-07 16:49:45 +0900

    Title: 5의 배수의 합, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 241007/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,8 +1,11 @@
 a, b = map(int, input().split())
 
+first = min(a, b)
+second = max(a, b)
+
 sum = 0
 
-for i in range(a, b+1):
+for i in range(first, second+1):
     if i % 5 == 0:
         sum += i
 
