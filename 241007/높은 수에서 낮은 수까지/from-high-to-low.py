333888b1317771584c6ebacb2db179e9cc0edf73
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-07 14:31:02 +0900

    Title: 높은 수에서 낮은 수까지, Time: 162ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 241007/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,9 +1,9 @@
 a, b = map(int, input().split())
 
-if a > b:
+if a >= b:
     for i in range(a, b - 1, -1):
         print(i, end = " ")
-elif a < b:
+elif a <= b:
     for i in range(b, a - 1, -1):
         print(i, end = " ")
 # for i in range(a, b, -1):
