f3c604838f174d4b6d2b5da469858ba60b10f19d
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-08 23:25:05 +0900

    Title: 조건에 따른 연산, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 241008/조건에 따른 연산/operatino-by-rule.py
+++ 241008/조건에 따른 연산/operatino-by-rule.py
@@ -1,10 +1,25 @@
+# n = int(input())
+# cnt = 0
+# while n < 1000:
+#     if n % 2 == 0:
+#         n = n * 3 + 1
+#     else:
+#         n = n * 2 + 1
+#     cnt += 1
+
+# print(cnt)
+
+# n을 입력받기
 n = int(input())
 cnt = 0
-while n < 1000:
-    if n % 2 == 0:
-        n = n * 3 + 1
-    else:
-        n = n * 2 + 1
-    cnt += 1
 
+# n이 1000 미만인 동안 반복
+while n < 1000:
+    if n % 2 == 0:  # 짝수인 경우
+        n = n * 3 + 1
+    else:  # 홀수인 경우
+        n = n * 2 + 2
+    cnt += 1  # 연산 횟수 증가
+
+# 결과 출력
 print(cnt)
