2e52d37b6aaf4917087e44e3316d99b98be23cfc
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-09 19:13:23 +0900

    Title: 소수 판별기, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/소수 판별기/prime-number-discriminator.py
+++ 241009/소수 판별기/prime-number-discriminator.py
@@ -1,33 +1,33 @@
-# def solution(n):
-#     satisfied = True
-
-#     for i in range(1, n+1):
-#         if n % i != 0:
-#             satisfied == False
-    
-#     if satisfied == True:
-#         print("P")
-#     else:
-#         print("C")
-
-# n = int(input())
-# solution(n)
-
 def solution(n):
-    # n이 소수인지 판별
-    if n < 2:  # 2보다 작은 수는 소수가 아님
-        print("C")
-        return
+    satisfied = True
 
-    # 2와 n은 소수이므로, 3부터 sqrt(n)까지 나누어보며 검사
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:  # 나누어 떨어지는 경우
-            print("C")
-            return
+    for i in range(1, n+1):
+        if n % i == 0:
+            satisfied == False
     
-    # 위의 조건을 모두 통과한 경우
-    print("P")
+    if satisfied == True:
+        print("P")
+    else:
+        print("C")
 
-# 입력 받기
 n = int(input())
 solution(n)
+
+# def solution(n):
+#     # n이 소수인지 판별
+#     if n < 2:  # 2보다 작은 수는 소수가 아님
+#         print("C")
+#         return
+
+#     # 2와 n은 소수이므로, 3부터 sqrt(n)까지 나누어보며 검사
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:  # 나누어 떨어지는 경우
+#             print("C")
+#             return
+    
+#     # 위의 조건을 모두 통과한 경우
+#     print("P")
+
+# # 입력 받기
+# n = int(input())
+# solution(n)
