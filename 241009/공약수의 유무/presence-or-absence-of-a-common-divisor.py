fee0706eef839e8e7cdfc7c0d51678ecbdce1bf9
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-09 18:57:02 +0900

    Title: 공약수의 유무, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 241009/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -12,22 +12,21 @@
 
 # a, b = map(int, input().split())
 # solution(a,b)
-
-import math
-
+# 변수 선언 및 입력
 def solution(a, b):
-    # 1920과 2880의 최대공약수를 구함
-    gcd_1920_2880 = math.gcd(1920, 2880)  # 결과는 960
-    
-    # 범위 내에 gcd_1920_2880의 배수가 있는지 확인
-    for i in range(a, b + 1):
-        if i % gcd_1920_2880 == 0:
-            print(0)  # 공약수가 존재함을 알림
-            return
-    
-    # 공약수가 없으면 0 출력
-    print(1)
+    satisfied = False
 
-# 입력 받기
+    for i in range(a, b + 1):
+        # a에서 b사이의 값 중 공약수가 있는지 확인합니다.
+        if 1920 % i == 0 and 2880 % i == 0:
+            satisfied = True
+            break  # 공약수를 찾으면 더 이상 반복할 필요 없음
+
+    # 출력
+    if satisfied:
+        print("1")
+    else:
+        print("0")
+
 a, b = map(int, input().split())
 solution(a, b)
