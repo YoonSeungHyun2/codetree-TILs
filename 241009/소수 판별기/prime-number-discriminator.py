bbdd9bc961f5c0e18026036106985bb8ff07e42d
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-09 19:13:34 +0900

    Title: 소수 판별기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/소수 판별기/prime-number-discriminator.py
+++ 241009/소수 판별기/prime-number-discriminator.py
@@ -3,7 +3,7 @@ def solution(n):
 
     for i in range(1, n+1):
         if n % i == 0:
-            satisfied == False
+            satisfied = False
     
     if satisfied == True:
         print("P")
