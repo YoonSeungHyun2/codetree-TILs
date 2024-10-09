884f2b8ab04d495136d5e8b6825ba6c8af4be24e
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-09 20:04:35 +0900

    Title: 정사각형 별표 출력하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241009/정사각형 별표 출력하기/print-stars-in-square.py
@@ -0,0 +1,15 @@
+def solution ():
+    for i in range(4):
+        for j in range(4):
+            print('*', end="")
+        print()
+
+solution()
+
+# import numpy as np
+
+# def solution():
+#     grid = np.full((4, 4), '*')  # 4x4 크기의 배열을 '*'로 초기화
+#     print(grid)  # 배열 출력
+
+# solution()
