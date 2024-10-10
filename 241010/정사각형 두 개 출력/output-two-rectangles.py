2fbf10c7b3703a8e6e53175dde4a0274af70d3f7
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-10 15:08:56 +0900

    Title: 정사각형 두 개 출력, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241010/정사각형 두 개 출력/output-two-rectangles.py
@@ -0,0 +1,13 @@
+def solution():
+    for _ in range(n):
+        for _ in range(n):
+            print("*", end = "")
+        print()
+    print()
+    for _ in range(n):
+        for _ in range(n):
+            print("*", end = "")
+        print()
+
+n = int(input())
+solution()
