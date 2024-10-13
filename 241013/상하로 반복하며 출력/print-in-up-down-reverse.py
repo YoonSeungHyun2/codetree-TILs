9fced6e4ae7ef5e6f58ead49be3fc9f42a804ff9
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-13 17:52:05 +0900

    Title: 상하로 반복하며 출력, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 241013/상하로 반복하며 출력/print-in-up-down-reverse.py
+++ 241013/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -7,3 +7,15 @@ for i in range(1, n+1):  # 1부터 n까지 행 반복
         else:  # 짝수 번째 열에서는 n - i + 1 출력
             print(n - i + 1, end="")
     print()  # 줄바꿈
+
+# # 변수 선언 및 입력
+# n = int(input())
+	
+# # 상하로 반복하여 출력합니다.
+# for i in range(n):
+# 	for j in range(n):
+# 		if j % 2 == 0:
+# 			print(i + 1, end="")
+# 		else:
+# 			print(n - i, end="")
+# 	print()
