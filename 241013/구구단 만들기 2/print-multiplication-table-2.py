6c5e0ab42f79c1e5afb4ba9b3c2b8f32ba1dd6f9
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-13 18:42:01 +0900

    Title: 구구단 만들기 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241013/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+
+# 큰 숫자부터 작은 숫자 순으로 짝수 구구단 출력
+for j in range(2, 10, 2): 
+    result = []
+    for i in range(b, 1, -1):  
+        result.append(f"{i} * {j} = {i * j}")
+    print(" / ".join(result))
