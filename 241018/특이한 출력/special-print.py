5c6fa413ab8099bfe81598aef40462280dd916cb
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-18 20:59:55 +0900

    Title: 특이한 출력, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/특이한 출력/special-print.py
+++ 241018/특이한 출력/special-print.py
@@ -1,6 +1,8 @@
-for i in range(1, 4): 
-    for j in range(1, 4):
-        if i % 2 == 0:
-            print(f"({i}, {j})", end="\n")
-        else:
-            print(f"({i}, {j})", end=" ")
+n = int(input())  # 사용자로부터 n 값을 입력받음
+
+for i in range(1, n + 1):  # i는 1부터 n까지
+    for j in range(1, n + 1):  # j는 1부터 n까지
+        print(f"({i}, {j})", end=" ")  # 각 좌표를 출력
+        if (i + j) % 4 == 0:  # i + j가 4의 배수이면
+            print()  # 줄을 바꿈
+    print()  # 각 i에 대해 줄을 바꿈 (다음 행으로 넘어가기 위해)
