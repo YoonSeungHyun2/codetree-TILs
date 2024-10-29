bb47af655afc80a234611c6f9cb4ad4f48f21dbc
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-10-29 18:47:30 +0900

    Title: ab곱 n번 구하기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241029/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+
+
+for _ in range(n):
+    
+    a, b = map(int, input().split())
+
+    prod_val = 1
+    for i in range(a, b + 1):
+        prod_val *= i
+    print(prod_val)
