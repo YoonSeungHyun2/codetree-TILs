ed440bdcf9078106bb3feefc0697967a3dbe6c3c
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-11-11 22:34:36 +0900

    Title: 학점 계산기, Time: 45ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241111/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+arr = list(map(int, input().split()))
+
+# 250 이하만 추출합니다.
+even_arr = []
+for num in arr:
+    if num >= 250:
+        break
+    even_arr.append(num)
+
+total_sum = sum(even_arr)
+average = round(total_sum / len(even_arr), 1)
+# 250 이하로만 이루어진 리스트
+print(total_sum, average)
