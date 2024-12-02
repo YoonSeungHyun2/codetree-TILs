7de993d2cfb2c7b2cec32ce63b9c63b599abfa35
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2024-12-02 14:31:26 +0900

    Title: 배열에서 원소의 특징, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241202/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
+++ 241202/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
@@ -1,11 +1,11 @@
-# 입력을 받아 리스트로 변환
-numbers = list(map(int, input().split()))
+# # 입력을 받아 리스트로 변환
+# numbers = list(map(int, input().split()))
 
-# 3의 배수가 처음으로 등장하는 위치를 찾음
-for i in range(1, len(numbers)):  # 인덱스 1부터 시작 (첫 번째 원소는 항상 3의 배수가 아님)
-    if numbers[i] % 3 == 0:
-        print(numbers[i - 1])  # 3의 배수의 바로 앞 원소 출력
-        break
+# # 3의 배수가 처음으로 등장하는 위치를 찾음
+# for i in range(1, len(numbers)):  # 인덱스 1부터 시작 (첫 번째 원소는 항상 3의 배수가 아님)
+#     if numbers[i] % 3 == 0:
+#         print(numbers[i - 1])  # 3의 배수의 바로 앞 원소 출력
+#         break
 
 # 배열에 주어진 수를 입력받아 저장합니다.
 arr = list(map(int, input().split()))
