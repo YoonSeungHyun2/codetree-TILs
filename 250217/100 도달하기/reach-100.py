ed703acd36307c50ccf88e1d2d8e3292c48bf177
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-02-17 14:45:36 +0900

    Title: 100 도달하기, Time: 51ms, Memory: 16MB, Status: Passed - CodeTree

--- 250217/100 도달하기/reach-100.py
+++ 250217/100 도달하기/reach-100.py
@@ -1,1 +1,13 @@
-sd
+n = int(input())  # 정수 n 입력받기
+
+a, b = 1, n  # 첫 번째 항: 1, 두 번째 항: n
+sequence = [a, b]  # 결과 저장 리스트
+
+while True:
+    next_term = a + b  # 다음 항 계산
+    sequence.append(next_term)  # 리스트에 추가
+    if next_term > 100:  # 100을 넘으면 종료
+        break
+    a, b = b, next_term  # 값 업데이트
+
+print(*sequence)  # 공백으로 구분해 출력
