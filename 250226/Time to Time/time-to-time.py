2c85d3cd3ad7901a95ce4fe25d427ef42fb2d946
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-02-26 19:29:54 +0900

    Title: Time to Time, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/Time to Time/time-to-time.py
+++ 250226/Time to Time/time-to-time.py
@@ -2,8 +2,39 @@
 
 # Please write your code here.
 
-h, m = 2, 5
+A, B, C, D = map(int, input().split())
 
-h2, m2 = 4, 1
 
-print((60 * h2 + m2) - (60 * h + m))
+h, m = A, B
+
+h2, m2 = C, D
+
+cnt = 0
+
+while True:
+    if h == h2 and m == m2:
+        break
+
+    m += 1
+    cnt += 1
+    if m == 60:
+        h += 1
+        m = 0
+
+print(cnt)
+
+# -------------------------
+
+# while not (h == h2 and m == m2):
+#     m += 1
+
+#     if m == 60:
+#         h += 1
+#         m = 0
+
+# -------------------------
+# h, m = 2, 5
+
+# h2, m2 = 4, 1
+
+# print((60 * h2 + m2) - (60 * h + m))
