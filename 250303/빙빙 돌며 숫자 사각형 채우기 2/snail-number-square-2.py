4b0fe67e5b2fedb2b48d32c143c60664d6b7a341
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-03-03 14:13:53 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250303/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+graph = [[0 for _ in range(m)] for _ in range(n)]
+
+
+# S -> E -> N -> W
+# dx = [0, 1, 0, -1]
+# dy = [1, 0, -1, 0]
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+d = 0
+
+
+x = 0
+y = 0
+
+
+for i in range(1, n*m+1):
+    graph[x][y] = i
+
+    nx = x + dx[d]
+    ny = y + dy[d]
+
+    if not (0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0):
+        d = (d + 1) % 4
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+    x = nx
+    y = ny
+
+
+for row in graph:
+    print(*row)
