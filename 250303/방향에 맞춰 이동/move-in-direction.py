da271072dcebf0b1e282b9f484e66e3ff01082b6
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-03-03 13:44:41 +0900

    Title: 방향에 맞춰 이동, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250303/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,27 @@
+# 0 : 동쪽 // 1: 남쪽 // 2: 서쪽 // 3:북쪽
+n = int(input())
+
+
+x, y = 0, 0
+
+# (1, 0): 동쪽으로 +1 // (0,-1): 남쪽으로 +1 // (-1,0) : 서쪽으로 +1 // (0,1): 북쪽으로 +1
+dx, dy = [1,0,-1,0], [0,-1,0,1]
+
+
+for _ in range(n):
+    d, s = tuple(input().split())
+    s = int(s)
+    if( d=='N'):
+        dir_num = 3
+    elif d=='S' :
+        dir_num = 1
+    elif d=='W':
+        dir_num = 2
+    elif d=='E':
+        dir_num = 0
+    for i in range(s):
+        nx, ny = x+dx[dir_num], y+ dy[dir_num]
+        x, y = nx, ny
+
+print(x, y)
+출처: https://dodenkr.tistory.com/65 [zheld.doden:티스토리]
