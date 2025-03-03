f9544acf624ae36a95abdaf38d7dd990c87df36c
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-03-03 13:59:03 +0900

    Title: 되돌아오기 2, Time: 55ms, Memory: 20MB, Status: Passed - CodeTree

---
+++ 250303/되돌아오기 2/come-back-2.py
@@ -0,0 +1,37 @@
+import sys
+
+
+input  = sys.stdin.readline
+
+
+x = 0
+y = 0
+d = 0
+
+
+direct = {"N":0, "E": 1, "S":2, "W":3}
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+cnt = 0
+
+cmd = list(input().rstrip())
+
+
+for c in cmd:
+    if c == 'L':
+        d = (d+3)%4
+    elif c == 'R':
+        d = (d+1)%4
+    else:
+        x = x + dx[d]
+        y = y + dy[d]
+        
+    cnt += 1
+
+    if x == 0 and y == 0:
+        print(cnt)
+        exit()
+
+print(-1)
