d8a588b25f32921551f0b60a6e062f6e3f31be37
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-03-03 14:16:28 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 126ms, Memory: 20MB, Status: Passed - CodeTree

--- 250303/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250303/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,38 +1,64 @@
-import sys
+#              1
+#              ^
+#              |      
+#      2 <-- (/,\) --> 0 
+#              | 
+#              v
+#              3
+
+n = int(input())
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y <n
+
+def move(dir_now):
+    if dir_now == 0:
+        return [0, 1]
+    elif dir_now == 1:
+        return [-1, 0]
+    elif dir_now == 2:
+        return [0, -1]
+    else:
+        return [1, 0]
+
+array = [[0 for i in range(n)]for j in range(n)] 
+
+dir_state = 3
+
+step = 1
+
+#왼쪽에 머가 없다면 돈다.
+
+x , y = (n-1)//2 , (n-1)//2
+
+i = 0
+
+array[x][y]=1
+
+while True:
+    #좌측 검사 포지션
+    dir_state_left = dir_state + 1
+    ldx, ldy = move(dir_state_left % 4)
+    lx , ly = x + ldx , y + ldy
+    #
+    if n != 1 and array[lx][ly] == 0:
+        dir_state += 1
+
+    dx, dy = move(dir_state % 4)
+
+    if in_range( x + dx ,y + dy) == True and array[x+dx][y+dy] == 0:
+        # 가는 방향이 맞고 0으로 돼있다면 진행한다
+        # 추가적으로 왼쪽에 숫자가 없다면 방향을 
+        x , y = x + dx , y + dy
+        step += 1
+
+    array[x][y] = step
+    if step >= n*n:
+        break
 
 
-input = sys.stdin.readline
-
-
-n, m = map(int, input().split())
-
-# E -> S -> W -> N
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-x = 0
-y = 0
-d = 0
-
-graph = [[0 for _ in range(m)] for _ in range(n)]
-
-for i in range(n*m):
-    graph[x][y] = chr((i%26) + 65)
-
-    nx = x + dx[d]
-    ny = y + dy[d]
-
-
-    if not( 0<= nx and nx < n and 0<= ny and ny <m and graph[nx][ny] == 0):
-        d = (d+1)%4
-        nx = x + dx[d]
-        ny = y + dy[d]
     
-    x = nx
-    y = ny
-
-
-for row in graph:
-    print(*row)
-print()
+for i in range(n):
+    for j in range(n):
+        print(array[i][j],end=" ")
+    print()
