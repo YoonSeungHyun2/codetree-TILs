f9ef1230d5ab8e431c2c3c7093f62d8b01e27522
Yoon SeungHyun <101793138+YoonSeungHyun2@users.noreply.github.com>
2025-03-03 13:57:43 +0900

    Title: 되돌아오기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250303/되돌아오기/come-back.py
+++ 250303/되돌아오기/come-back.py
@@ -1,37 +1,30 @@
 import sys
 
+input = sys.stdin.readline
 
-input  = sys.stdin.readline
-
-
-x = 0
+x = 0 
 y = 0
-d = 0
 
-
-direct = {"N":0, "E": 1, "S":2, "W":3}
+direct = {"N": 0, "E": 1, "S": 2, "W": 3}
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
+
+n = int(input())
 cnt = 0
 
-cmd = list(input().rstrip())
+for _ in range(n):
+    di, move = input().rstrip().split(" ")
+    move = int(move)
 
+    for m in range(move):
+        x = x + dx[direct[di]]
+        y = y + dy[direct[di]]
+        cnt += 1
 
-for c in cmd:
-    if c == 'L':
-        d = (d+3)%4
-    elif c == 'R':
-        d = (d+1)%4
-    else:
-        x = x + dx[d]
-        y = y + dy[d]
-        
-    cnt += 1
-
-    if x == 0 and y == 0:
-        print(cnt)
-        exit()
+        if x == 0 and y == 0:
+            print(cnt)
+            exit()
 
 print(-1)
